28ae00a2356418426d2e34cea5d346a0ed494d55
Suuuuuukang <18834839360@163.com>
2023-05-08 22:50:02 +0800

    first commit

--- src/api/picture_api.js
+++
@@ -1,8 +0,0 @@
-import http from "./http";
-
-export default {
-  async getCos (page) {
-    let res = http.get('https://v.api.aa1.cn/api/api-tplist/go.php/api/picture/index?page=' + page)
-    return res
-  }
-}
